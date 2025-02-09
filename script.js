33177cea0c23d20cf89ffe2524cf58e5c626704a
470M <165724285+EthanDupe@users.noreply.github.com>
2025-02-08 17:39:50 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,48 @@
+// Function to close a window by hiding it
+function closeWindow(windowId) {
+  document.getElementById(windowId).style.display = 'none';
+}
+
+// Make the window draggable
+dragElement(document.getElementById("window1"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  // Use the title bar as the drag handle if available
+  var titleBar = elmnt.querySelector(".title-bar");
+  if (titleBar) {
+    titleBar.onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
